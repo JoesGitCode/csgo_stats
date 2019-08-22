7fca5b63cdfee0fdc96a5863dc00c611f9726b72
Joseph Young <jsyoung0891@gmail.com>
2019-08-22 18:58:31 +0100

    front and back running concurrently

--- server.js
+++ server.js
@@ -11,13 +11,25 @@ app.get("/api/v1/profile/steam/:steamid", async (req, res) => {
   const apiUrl = process.env.API_URL;
   const { steamid } = req.params;
 
-  const response = await fetch(`${apiUrl}/profile/steam/${steamid}`, {
-    headers
-  });
+  try {
+    const response = await fetch(`${apiUrl}/profile/steam/${steamid}`, {
+      headers
+    });
 
-  const data = await response.json();
+    const data = await response.json();
 
-  res.json(data);
+    if (data.errors && data.errors.length > 0) {
+      return res.status(404).json({
+        message:
+          "Profile not found. Try typing your ID again or make you steam account public"
+      });
+    }
+
+    res.json(data);
+  } catch (error) {
+    console.error(error, "Server error");
+    res.status(500);
+  }
 });
 
 const port = process.env.PORT;
