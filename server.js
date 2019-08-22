d8998cee612d72cad893548dd5e810e876111028
Joseph Young <jsyoung0891@gmail.com>
2019-08-22 16:48:23 +0100

    gets correct response from csgo api

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const fetch = require("node-fetch");
 
 dotenv.config({ path: "./config.env" });
 
 const app = express();
+
+app.get("/api/v1/profile/steam/:steamid", async (req, res) => {
+  const headers = { "TRN-Api-Key": process.env.API_KEY };
+  const apiUrl = process.env.API_URL;
+  const { steamid } = req.params;
+
+  const response = await fetch(`${apiUrl}/profile/steam/${steamid}`, {
+    headers
+  });
+
+  const data = await response.json();
+
+  res.json(data);
+});
 
 const port = process.env.PORT;
 
